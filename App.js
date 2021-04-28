568ab240985f6ed1799888014f93debc0f061cad
Achintha Ikiriwatte <achintha.ikiriwatte@outlook.com>
2021-04-28 14:57:30 +0530

    added logic to search coins

--- App.js
+++ App.js
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
     flex: 1
   },
   header: {
-    marginVertical: 8,
+    marginTop: 15,
     flexDirection: 'row',
     justifyContent: 'center'
   },
--- src/container/List.js
+++ src/container/List.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet, TextInput } from "react-native";
 import { connect } from "react-redux";
-import { fetchCoinList } from "../actions";
+import { fetchCoinList, searchCoinList } from "../actions";
 import CoinCard from "../components/CoinCard/CoinCard";
+import EmptyList from "../components/EmptyList/EmptyList";
 
 const List = props => {
 
-    const [number, onSearchChange] = React.useState(null);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
         props.fetchCoinList();
@@ -20,11 +21,16 @@ const List = props => {
         props.fetchCoinList();
     }
 
+    const onSearchChange = (value) => {
+        props.searchCoins(value)
+    }
+
     return (
         <>
             <TextInput
                 style={props.theme ? styles.inputLight : styles.inputDark}
-                onChangeText={onSearchChange}
+                value={search}
+                onChangeText={(e) => { onSearchChange(e), setSearch(e) }}
                 placeholder="Search"
                 placeholderTextColor={props.theme ? "#000" : "#fff"}
             />
@@ -37,6 +43,7 @@ const List = props => {
                 keyExtractor={(item, index) => index.toString()}
                 onRefresh={() => _onRefresh()}
                 refreshing={props.loading}
+                ListEmptyComponent={props.loading ? "" : <EmptyList theme={props.theme} />}
             />
         </>
     );
@@ -46,6 +53,9 @@ const mapDispatchToProps = dispatch => {
     return {
         fetchCoinList: () => {
             dispatch(fetchCoinList());
+        },
+        searchCoins: (query) => {
+            dispatch(searchCoinList(query))
         }
     };
 };
@@ -64,16 +74,20 @@ export default connect(
 
 const styles = StyleSheet.create({
     inputDark: {
-        marginTop: 0,
-        margin: 12,
+        marginBottom: 12,
+        marginLeft: 12,
+        marginRight: 12,
         borderBottomWidth: 0.2,
         borderBottomColor: "#fff",
-        color: '#fff'
+        color: '#fff',
+        fontSize: 18
     },
     inputLight: {
-        marginTop: 0,
-        margin: 12,
+        marginBottom: 12,
+        marginLeft: 12,
+        marginRight: 12,
         borderBottomWidth: 0.2,
-        borderBottomColor: "#000"
+        borderBottomColor: "#000",
+        fontSize: 18
     },
 });
